c727b637ec7e4a84eca47ccf723a6167b81f76a4
susices <susices@hotmail.com>
2022-11-07 17:30:46 +0800

    Add Logging System

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,7 +5,10 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
-	printf("Hazel Engine");
+	printf("Init Hazel Engine\n");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Init Log!");
+	HZ_INFO("Hello {}  Haha!","YY");
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;

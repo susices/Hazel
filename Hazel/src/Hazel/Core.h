0b83f5eb4f595321c7c343ef7100633e4d272519
susices <susices@hotmail.com>
2022-11-14 16:59:39 +0800

    update premake

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -10,9 +10,13 @@
 	#error Hazel only support Windows!
 #endif // HZ_PLATFORM_WINDOWS
 
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif
+
 # ifdef  HZ_ENABLE_ASSERTS
-	#define HZ_ASSERT(x,...) {if (!x){HZ_ERROR("Assertion Falied: {0}", __VA_ARGS); __debugbreak();}}
-	#define HZ_CORE_ASSERT(x,...) {if(!(x)) {HZ_CORE_ERROR("Assertion Failed: {0}",__VA_ARGS__); __debugbreak();}}
+	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
 	#define HZ_ASSERT(x,...)
 	#define HZ_CORE_ASSERT(x,...)
